32b0bda78605c7b13405db32e781db91a4d868b2
Len-Kedrow <Len.Kedrow@gmail.com>
2024-06-28 17:44:34 -0400

    creating a git repository for team practice

---
+++ react-pokemon/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import PokeButton from './componenets/PokeButton'
+
+function App() {
+  return (
+    <>
+      <p>Len and Rachel PokeGals!!!</p>
+      <PokeButton></PokeButton>
+    </>
+  )
+}
+
+export default App
+
+
