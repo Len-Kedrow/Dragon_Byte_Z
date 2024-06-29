81ba1033a2314bef23e5b8662c167e16d4552957
Len-Kedrow <Len.Kedrow@gmail.com>
2024-06-29 17:54:12 -0400

    created folder for DB team project

--- react-pokemon/vite-project/src/App.jsx
+++ react-pokemon/vite-project/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import PokeButton from './componenets/PokeButton'
+import pokedex from "src/assets/pokedex.png"
 
 function App() {
   return (
     <>
-      <p>Len and Rachel PokeGals!!!</p>
+    <div><img src = {pokedex}></img></div>
+      <p>PokePals!!!</p>
       <PokeButton></PokeButton>
     </>
   )
--- react-pokemon/vite-project/src/componenets/PokeButton.jsx
+++ react-pokemon/vite-project/src/componenets/PokeButton.jsx
@@ -3,56 +3,34 @@ import axios from "axios"
 
     function PokeButton() {
         
-      //const [type, setType] = useState("");      
+      const [type, setType] = useState(""); //This will hold the typ of Pokemon we pull      
       const [pokeImg, setPokeImg] = useState("")
-      //const catchEmAll = []
+      const catchEmAll = []
 
-      
-
-      
-        
-      // const getPokemon = async () => { /* async will wait until API is called*/
-      //   try {                         /* the response variable will have the fetched api  */
-      //     let response = await axios.get("https://pokeapi.co/api/v2/pokemon/charizard");
-      //     (setType(response))
-      //     console.log(response)
-          
-      //   } catch (error) {
-      //     console.error("An error occurred:", error)
-      //   }
-        
-      // }
-
-
-      /* as soon as you hit NPM RUN DEV, it will IMMEDIATELY FETCH THE IMAGE! BEHIND THE CURTAIN! */
-      /* ON CLICK, YOU WILL CALL ANOTHER FUNCTION THAT WILL REVEAL THE API CALL */
-      /* using useEffect to fetch api to set the pokemon Type  */
-      async function FetchData() {
-        try {
+      //function to get a random Pokemon. Need to build out so builds team of single type
+      const FetchData = async () => { 
+        try {                         
           let randomNum = Math.floor(Math.random() * 151); + 1// random num 1-100
-          let response = await axios.get(
-            `https://pokeapi.co/api/v2/pokemon/${randomNum}`
-          );
-          //console.log(response);
+          let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`);
+          (setType(response))
+          console.log(response)
           setPokeImg(response.data.sprites.front_default);
         } catch (error) {
-          console.error("An error occurred:", error);
+          console.error("An error occurred:", error)
         }
+        
       }
       
-      useEffect( () => { /* runs on render*/                                  /* len "i will give you this 5$ rachel, idk when. "*/
+      //On render FetchData() will grab the first team of Pokemon
+      useEffect( () => { 
          FetchData();
       }, [])
       
+      //Used to reveal pokemon to screen
       const curtainReveal = () => {
         let ImgElement = document.getElementById("insertPokemon")
         ImgElement.src = pokeImg
-        // let container = document.getElementById("container");
-        // container.innerHTML = '<img src={pokeImg}></img>';
-        // let ElementImg = document.createElement('img')
-       
-        // ElementImg.src = pokeImg //link
-        //container.append(ElementImg) //link
+      
       }
     
     return (
